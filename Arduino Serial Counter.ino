0ae12d1b0592fc95d3b28a4870910577d8c26dda
oodabasi <104134641+oodabasi@users.noreply.github.com>
2022-04-25 17:21:37 +0300

    Update Arduino Code

--- Arduino Serial Counter.ino
+++ Arduino Serial Counter.ino
@@ -1,14 +1,45 @@
-int counter = 0;
+int start_time;
+int countdown;
+bool countdown_active = 0;
 
 void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);   
     Serial.begin(9600);
     Serial.setTimeout(50);
-    Serial.println("Comms Started");
+    Serial.println("Comms Connected");
 }
 
 void loop() {   
-  if(Serial.available() > 0 && Serial.readString() == "Button Pressed\n"){
-    counter++;
-    Serial.println(counter);
-  }  
+  int current_time = millis();
+  if(countdown_active && (current_time - start_time) >= 1000){    
+    Serial.println(countdown);
+    countdown--;
+    if(countdown == -1){
+      countdown_active = 0;
+    }
+    else{
+      start_time = current_time;      
+    }    
+  }
+    
+  if(Serial.available() > 0){    
+    String command = Serial.readString();    
+    if(command == "Led On\n"){
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else if(command == "Led Off\n"){
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+    else if(command == "Stop\n"){
+      countdown = 0;
+      countdown_active = 0;
+    }
+    else{
+      countdown = command.toInt();
+      countdown_active = 1;
+            
+    }  
+  }     
 }
+
+  
